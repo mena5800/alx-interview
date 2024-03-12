69d9eaef24852d67f8bd463d04b1241411be5fbc
mena5800 <mr.safwat55@gmail.com>
2024-03-12 15:24:47 +0200

    fix documentation of 0x01. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
+"""
+    this file include the solution of probelm 0x01. Lockboxes
+
+    problem statement:
+      You have n number of locked boxes in front of you.
+      Each box is numbered sequentially from 0 to n - 1 and
+      each box may contain keys to the other boxes.
+"""
+
 
 def canUnlockAll(boxes):
+    """
+    method that determines if all the boxes can be opened.
+
+    Args:
+      boxes : is a list of lists
+
+    Return:
+      True if all boxes can be opened, else return False
+
+    """
     visited = set([0])
     queue = [boxes[0]]
 
