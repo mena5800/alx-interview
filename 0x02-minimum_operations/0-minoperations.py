4771935ddafd952bf0e686bac4a8d18b53c7c492
mena5800 <mr.safwat55@gmail.com>
2024-03-20 12:55:16 +0200

    fix documentation of 0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 """
-    this file include the solution of probelm 0x01. Lockboxes
+    this file include the solution of probelm 0. Minimum Operations
 
     problem statement:
-      You have n number of locked boxes in front of you.
-      Each box is numbered sequentially from 0 to n - 1 and
-      each box may contain keys to the other boxes.
+      In a text file, there is a single character H.
+      Your text editor can execute only two operations
+      in this file: Copy All and Paste. Given a number n,
+      write a method that calculates the fewest number of
+      operations needed to result in exactly n H characters
+      in the file.
 """
 from functools import cache
 
 
 def minOperations(n):
     """
-    method that determines if all the boxes can be opened.
+    method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
 
     Args:
-      boxes : is a list of lists
+      n : int
 
     Return:
-      True if all boxes can be opened, else return False
+      num If n is impossible to achieve, return 0
 
     """
 
