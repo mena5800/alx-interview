d0935d3c0a6a7e1aa6fa8a505950f1e4d25cd3e4
mena5800 <mr.safwat55@gmail.com>
2024-06-30 18:04:27 +0300

    solve validate utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""this file contain a method that determines if a given
+data set represents a valid UTF-8 encoding."""
+
+
+def validUTF8(data: list[int]) -> bool:
+    """
+    method that determines if a given data set
+    represents a valid UTF-8 encoding or not.
+
+    Args:
+      data : list of integers,
+            each integer represents byte encoding in utf-8.
+    Return:
+      bool : True if valid utf otherwise False.
+
+    """
+    n_bits = 0
+    for num in data:
+        if n_bits > 0:
+            # check the msb is 1 and 7th bit is 0
+            if ((num >> 7) and ((num >> 6) & 1) == 0):
+                n_bits -= 1
+            else:
+                return False
+        elif num <= 127:
+            continue
+        else:
+            i = 6
+            while ((num >> i) & 1):
+                n_bits += 1
+                i -= 1
+            if n_bits == 0:
+                return False
+    return True if n_bits == 0 else False
